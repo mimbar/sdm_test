21534946d01972b10c13a758c9ee2f8c777ba3cd
Mimbar Adi Nugraha <mimbar.muse@gmail.com>
2019-07-30 21:17:07 +0700

    revert to old css

--- Modules/Home/Resources/views/home/dashboard.blade.php
+++ Modules/Home/Resources/views/home/dashboard.blade.php
@@ -2,8 +2,30 @@
 
 @section('main')
     <div class="content">
-        <div class="d-flex justify-content-around row align-self-stretch">
+        <div class="card">
+            <div class="card-header header-elements-inline">
+                <h5 class="card-title">Periode</h5>
+                <div class="header-elements">
+                    <button class="btn bg-success" data-toggle="modal" data-target="#modaladdPeriode">
+                        <i class="icon-add"></i>
+                        Tambah
+                    </button>
+                </div>
+            </div>
 
+
+            <table class="table datatable" style="width:100%">
+                <thead>
+                <tr>
+                    <th class="text-center">Kode Tahun</th>
+                    <th class="text-center">Program Studi</th>
+                    <th class="text-center">Deskripsi</th>
+                    <th class="text-center">Tanggal Mulai</th>
+                    <th class="text-center">Tanggal Selesai</th>
+                    <th class="text-center">Aksi</th>
+                </tr>
+                </thead>
+            </table>
         </div>
     </div>
 @endsection
